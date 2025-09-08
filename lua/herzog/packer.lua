e2e40d6b27476c7debd6742b7bd0833be2592e13
ZachWi <constellationagram@gmail.com>
2025-09-08 13:23:09 -0400

    ggvG= lmfao

--- lua/herzog/packer.lua
+++ lua/herzog/packer.lua
@@ -4,40 +4,40 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use { "catppuccin/nvim", as = "catppuccin" }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { "catppuccin/nvim", as = "catppuccin" }
 
-  use({
-	  'folke/tokyonight.nvim',
-	  as = 'tokyonight',
-	  config = function()
-		  vim.cmd.colorscheme "catppuccin"
-	  end
-  })
-  use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = function()
-		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-		  ts_update()
-	  end,
-  }
-  use {
-	  "ThePrimeagen/harpoon",
-	  branch = "harpoon2",
-	  requires = { {"nvim-lua/plenary.nvim"} }
-  }
-  use 'neovim/nvim-lspconfig' -- Core LSP client
-  use 'williamboman/mason.nvim' -- Language server installer
-  use 'williamboman/mason-lspconfig.nvim' -- Integrates Mason with nvim-lspconfig
-  use 'VonHeikemen/lsp-zero.nvim' -- LSP Zero itself
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'folke/neodev.nvim'
+    use({
+        'folke/tokyonight.nvim',
+        as = 'tokyonight',
+        config = function()
+            vim.cmd.colorscheme "catppuccin"
+        end
+    })
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { {"nvim-lua/plenary.nvim"} }
+    }
+    use 'neovim/nvim-lspconfig' -- Core LSP client
+    use 'williamboman/mason.nvim' -- Language server installer
+    use 'williamboman/mason-lspconfig.nvim' -- Integrates Mason with nvim-lspconfig
+    use 'VonHeikemen/lsp-zero.nvim' -- LSP Zero itself
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'folke/neodev.nvim'
 end)
