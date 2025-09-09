951e8fa5063b974144d62a680e477dfbcf5481d5
ZachWi <constellationagram@gmail.com>
2025-09-09 12:07:38 -0400

    updated motorbike

--- after/plugin/motorbike.lua
+++ after/plugin/motorbike.lua
@@ -6,7 +6,6 @@ vim.keymap.set("n", "<leader>sd", function()
     motorbike.swap()
 end, { desc = "Swap motorbike directories" })
 
--- Set directories manually (example)
 vim.keymap.set("n", "<leader>sm", function()
     motorbike.set_directories(
         "/Users/zacharywillis/motorbike/",
