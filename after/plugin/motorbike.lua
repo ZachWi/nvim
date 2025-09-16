0fe80788fa0fc528122bb6f02e2d80fc807a487b
ZachWi <constellationagram@gmail.com>
2025-09-16 18:29:55 -0400

    changed motorbike config

--- after/plugin/motorbike.lua
+++ after/plugin/motorbike.lua
@@ -2,14 +2,8 @@ require("motorbike")
 local motorbike = require("motorbike")
 
 -- Swap directories
-vim.keymap.set("n", "<leader>sd", function()
-    motorbike.swap()
-end, { desc = "Swap motorbike directories" })
-
-vim.keymap.set("n", "<leader>sm", function()
-    motorbike.set_directories(
+vim.keymap.set("n", "<leader>sd", function() motorbike.swap() end)
+vim.keymap.set("n", "<leader>sm", function() motorbike.set_directories(
         "/Users/zacharywillis/motorbike/",
         "/Users/zacharywillis/.config/nvim/"
-    )
-end, { desc = "Set motorbike directories" })
-
+    ) end)
